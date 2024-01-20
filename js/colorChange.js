453877cc3146fdf563fab94b74833b4545885ff6
Kristina <53626966+Kristi-T@users.noreply.github.com>
2024-01-20 22:44:52 +0200

    Add color and text change section; add functions; removed js scripts to new folder

---
+++ js/colorChange.js
@@ -0,0 +1,47 @@
+// Change color and text
+var divElement1 = document.getElementById("divFuchsia");
+var divElement2 = document.getElementById("divOrange");
+var changeColorButton = document.getElementById("changeColorButton");
+var changeTextButton = document.getElementById("changeTextButton");
+
+function changeColor() {
+
+  if (divElement1.className == "bgColorFuchsia") {
+    divElement1.className = "bgColorBlue";
+  } else {
+    divElement1.className = "bgColorFuchsia";
+  }
+
+  if (divElement2.className == "bgColorOrange") {
+    divElement2.className = "bgColorYellow";
+  } else {
+    divElement2.className = "bgColorOrange";
+  }
+
+  if (divElement1.className == "bgColorFuchsia") {
+    changeColorButton.value = "Change color";
+  } else {
+    changeColorButton.value = "Reset color";
+  }
+
+}
+
+function changeText() {
+  // divElement1.querySelector("p").innerHTML = "Glory Ukraine!!!";
+  // divElement2.querySelector("p").innerHTML = "To the heroes Glory!!!";
+  
+  if (divElement1.querySelector("p").innerHTML === "Glory Ukraine!!!") {
+    changeTextButton.value = "Reset text";
+    divElement1.querySelector("p").innerHTML = "Hello World!";
+  } else {
+    divElement1.querySelector("p").innerHTML = "Glory Ukraine!!!";
+    changeTextButton.value = "Change text";
+  }
+
+  if (divElement2.querySelector("p").innerHTML === "To the heroes Glory!!!") {
+    divElement2.querySelector("p").innerHTML = "Nice to meet you!";
+  } else {
+    divElement2.querySelector("p").innerHTML = "To the heroes Glory!!!";
+  }
+
+}
