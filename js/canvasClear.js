1f28a9ce263345b260fe75ed53088c9bbce9a37d
Kristina <53626966+Kristi-T@users.noreply.github.com>
2024-01-27 12:27:20 +0200

    Update section with canvas
    add text input; add color changing

--- js/canvasClear.js
+++ js/canvasClear.js
@@ -1,34 +1,49 @@
+var canvas1 = document.getElementById("canvasOrange");
+var canvas2 = document.getElementById("canvasPink");
+var ctx1 = canvas1.getContext("2d");
+var ctx2 = canvas2.getContext("2d");
+
+
 function changeCanvasColor() {
-	var canvas1 = document.getElementById("canvasOrange");
-	var canvas2 = document.getElementById("canvasPink");
 	canvas1.className = "greyback";
 	canvas2.className = "greenback";
-	
 }
 
 function doPink()
 {
-	var canvas2 = document.getElementById("canvasPink");
 	canvas2.className = "bgColorFuchsia";
-	
 }
 
 function doOrange()
 {
-	var canvas1 = document.getElementById("canvasOrange");
 	canvas1.className = "bgColorOrange";
-	
 }
 
 function clearCanvas() {
-	var canvas1 = document.getElementById("canvasOrange");
-	var canvas2 = document.getElementById("canvasPink");
 	canvas1.className = "greyback";
 	canvas2.className = "greenback";
-	var ctx1 = canvas1.getContext("2d");
-	var ctx2 = canvas2.getContext("2d")
 	ctx1.clearRect(0, 0, canvas1.clientWidth, canvas1.height);
 	canvas1.className = "transparent";
+	ctx1.clearRect(0, 0, canvas1.width, canvas1.height); 
 	ctx2.clearRect(0, 0, canvas2.clientWidth, canvas2.height);
 	canvas2.className = "transparent";
+}
+
+function addTextViaInput() {
+	var inputElement = document.getElementById("inputCanvasText");
+  var text = inputElement.value;
+  var maxLength = 25;
+
+  if (text.length > maxLength) {
+    inputElement.value = text.slice(0, maxLength); // Обрізати текст до максимальної довжини
+  }
+	var text1 = document.getElementById("inputCanvasText").value;
+	ctx1.clearRect(0, 0, canvas1.width, canvas1.height); // Очищаємо канвас перед виведенням нового тексту
+	ctx1.font = "18px Arial";
+	ctx1.textAlign = "center";
+	ctx1.textBaseline = "middle";
+	var centerW = canvas1.width / 2; 
+	var centerH = canvas1.height / 2; 
+	ctx1.fillText(text1, centerW, centerH);
+	ctx1.strokeText(text1, centerW, centerH);
 }
